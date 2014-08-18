7a038e0f232ad4b92db8f80dd54d1d92defa0d5c
Matt Friedman <maf675@gmail.com>
2014-08-18 09:35:32 -0700

    Add composer and ext.php files

---
+++ ext.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*
+* Pages extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace phpbb\pages;
+
+/**
+* This ext class is optional and can be omitted if left empty.
+* However you can add special (un)installation commands in the
+* methods enable_step(), disable_step() and purge_step(). As it is,
+* these methods are defined in \phpbb\extension\base, which this
+* class extends, but you can overwrite them to give special
+* instructions for those cases.
+*/
+class ext extends \phpbb\extension\base
+{
+}
