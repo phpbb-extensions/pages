9988480dcb7df452a560c3e0d0c28328c814a74b
Matt Friedman <maf675@gmail.com>
2014-08-20 18:36:30 -0700

    Add (empty) event listener file

---
+++ event/listener.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* Pages extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace phpbb\pages\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+}
