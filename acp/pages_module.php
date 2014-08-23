c371b526170020c55db2c19f8c66776a9925b2bf
Pico88 <luki_9@wp.pl>
2014-08-23 06:34:40 +0200

    Add page acp

---
+++ acp/pages_module.php
@@ -0,0 +1,74 @@
+<?php
+/**
+*
+* Pages extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace phpbb\pages\acp;
+
+class pages_module
+{
+	public $u_action;
+
+	function main($id, $mode)
+	{
+		global $phpbb_container, $request, $user;
+
+		// Add the board rules ACP lang file
+		$user->add_lang_ext('phpbb/pages', 'pages_acp');
+
+		// Get an instance of the admin controller
+		$admin_controller = $phpbb_container->get('phpbb.pages.admin.controller');
+
+		// Requests
+		$action = $request->variable('action', '');
+		$page_id = $request->variable('page_id', 0);
+
+		// Make the $u_action url available in the admin controller
+		$admin_controller->set_page_url($this->u_action);
+
+		// Load a template from adm/style for our ACP page
+		$this->tpl_name = 'pages_manage';
+
+		// Set the page title for our ACP page
+		$this->page_title = $user->lang('ACP_PAGES_MANAGE');
+
+		// Perform any actions submitted by the user
+		switch($action)
+		{
+			case 'add':
+				// Set the page title for our ACP page
+				$this->page_title = $user->lang('ACP_PAGES_CREATE_PAGE');
+
+				// Load the add page handle in the admin controller
+				$admin_controller->add_page();
+
+				// Return to stop execution of this script
+				return;
+			break;
+
+			case 'edit':
+				// Set the page title for our ACP page
+				$this->page_title = $user->lang('ACP_PAGES_EDIT_PAGE');
+
+				// Load the edit page handle in the admin controller
+				$admin_controller->edit_page($page_id);
+
+				// Return to stop execution of this script
+				return;
+			break;
+
+			case 'delete':
+				// Delete a page
+				$admin_controller->delete_page($page_id);
+			break;
+		}
+
+		// display the language selection menu.
+		$admin_controller->display_pages();
+	}
+}
