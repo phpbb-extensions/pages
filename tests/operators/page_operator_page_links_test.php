086ef12512c873e62a065646ad9bfb69fddf76c1
Matt Friedman <maf675@gmail.com>
2014-09-03 03:10:39 -0700

    Combine page links tests into 1 file

--- tests/operators/page_operator_page_links_test.php
+++ tests/operators/page_operator_page_links_test.php
@@ -146,4 +146,68 @@ class page_operator_get_page_links_test extends page_operator_base
 
 		$this->assertEquals($expected, $result);
 	}
+
+	/**
+	* Test data for the test_add_page_links() function
+	*
+	* @return array Array of test data
+	* @access public
+	*/
+	public function add_page_links_test_data()
+	{
+		return array(
+			array(1, array(1, 2), true),
+			array(2, array(2), true),
+			array(3, array(), false), // no data to add
+		);
+	}
+
+	/**
+	* Test adding page links
+	*
+	* @dataProvider add_page_links_test_data
+	* @access public
+	*/
+	public function test_add_page_links($page_id, $link_ids, $expected)
+	{
+		// Setup the operator class
+		$operator = $this->get_page_operator();
+
+		// Add the page links
+		$result = $operator->insert_page_links($page_id, $link_ids);
+
+		// Assert the page link data was added
+		$this->assertEquals($expected, $result);
+	}
+
+	/**
+	* Test data for the test_add_page_links_fails() function
+	*
+	* @return array Array of test data
+	* @access public
+	*/
+	public function add_page_links_fails_test_data()
+	{
+		return array(
+			array('', array(1, 2)),
+			array(100, array(1, 2)),
+			array(null, array(1, 2)),
+		);
+	}
+
+	/**
+	* Test adding page links fails
+	*
+	* @dataProvider add_page_links_fails_test_data
+	* @expectedException \phpbb\pages\exception\base
+	* @access public
+	*/
+	public function test_add_page_links_fails($page_id, $link_ids)
+	{
+		// Setup the operator class
+		$operator = $this->get_page_operator();
+
+		// Add the page links
+		$operator->insert_page_links($page_id, $link_ids);
+	}
 }
