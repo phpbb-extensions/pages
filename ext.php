d49fe05d1202538ebbb213750045ab55839a8136
Matt Friedman <maf675@gmail.com>
2015-05-30 18:41:28 -0700

    Add an ext.php with 3.2 phpBB requirement

---
+++ ext.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ * Pages extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace phpbb\pages;
+
+/**
+* This ext class is optional and can be omitted if left empty.
+* However you can add special (un)installation commands in the
+* methods enable_step(), disable_step() and purge_step(). As it is,
+* these methods are defined in \phpbb\extension\base, which this
+* class extends, but you can overwrite them to give special
+* instructions for those cases.
+*/
+class ext extends \phpbb\extension\base
+{
+	/**
+	 * Check whether or not the extension can be enabled.
+	 * The current phpBB version should meet or exceed
+	 * the minimum version required by this extension:
+	 *
+	 * Requires phpBB 3.2.0-dev due to new faq controller route for bbcodes
+	 *
+	 * @return bool
+	 * @access public
+	 */
+	public function is_enableable()
+	{
+		return phpbb_version_compare(PHPBB_VERSION, '3.2.0-dev', '>=');
+	}
+}
