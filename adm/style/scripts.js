71ece2c4c3bc68e5c017bd0b111466e67ebd64bb
Matt Friedman <maf675@gmail.com>
2015-12-01 14:36:31 -0800

    Auto Suggest route based on title field input

--- adm/style/scripts.js
+++ adm/style/scripts.js
@@ -15,6 +15,13 @@ $(function() {
 	$('.html-on').toggleBoxes('.html-off');
 	$('.html-off').toggleBoxes('.html-on');
 
+	$('#page_title').on('blur', function() {
+		var title = $(this).val();
+		$('#page_route').val(function(event, route) {
+			return (route) ? route : title.toLowerCase().replace(/[^a-z0-9-_\s]/gi, '').replace(/[\s]/g, '-');
+		});
+	});
+
 });
 
 })(jQuery); // Avoid conflicts with other libraries
